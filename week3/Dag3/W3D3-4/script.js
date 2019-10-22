b352da1eaa5655fbf271bc9f4d9f5c3a1678a0e5
Eurvin Morman <eurvin@gmail.com>
2019-10-22 14:03:39 +0200

    improved core functionality ;-)

--- week3/Dag3/W3D3-4/script.js
+++ week3/Dag3/W3D3-4/script.js
@@ -1,8 +1,55 @@
-getMovies()
-.then(data => {
-  let body = document.querySelector('body');
-  let pre = document.createElement('pre');
-  let code = document.createElement('code');
-  body.appendChild(pre).appendChild(code)
-  code.textContent = JSON.stringify(data, null, " ");
-})
+getMovieGenres('/genre/movie/list', API_KEY, 'GET').then(responseJSON => {
+    const movies = responseJSON;
+    setMovieGenres(movies);
+});
+
+getMovieById('/movie/420818', API_KEY, 'GET').then(responseJSON => {
+    const movies = responseJSON;
+    setMovieById(movie);
+}); // the lionking
+getMovieById('/movie/475557', API_KEY, 'GET') // joker
+getMovieById('/movie/301528', API_KEY, 'GET') // toy story 4
+
+getTopRatedMovies('/movie/top_rated', API_KEY, 'GET').then(responseJSON => {
+    const topRated = responseJSON;
+    setTopRatedMovies(topRated);
+});
+
+// getTopRatedActionMovies('/movie/top_rated', API_KEY, 'GET').then(responseJSON => {
+//     const movies = responseJSON
+//     console.log(movies);
+//     setMovieGenres(movies)
+//   console.log("hier is je data om iets mee te gaan doen:", responseJSON);
+// });
+
+const setMovieGenres = function(movieData){
+    const moviesGenres = movieData.genres;
+    moviesGenres.forEach(movieGenre => {
+        const listItem = document.createElement('li');
+        const listItemContent = `genre naam: ${movieGenre.name}, id: ${movieGenre.id}`
+        listItem.appendChild(document.createTextNode(listItemContent));
+        document.getElementById("movieGenresList").append(listItem)
+    })
+}
+
+const setTopRatedMovies = function(movieData){
+    const topRatedMovies = movieData['results'];
+    console.log(topRatedMovies);
+    topRatedMovies.forEach(topRatedMovie => {
+        const listItem = document.createElement('li');
+        const listItemContent = `Film naam: ${topRatedMovie.title}, Gem. rating: ${topRatedMovie.vote_average}`
+        listItem.appendChild(document.createTextNode(listItemContent));
+        document.getElementById("topRatedList").append(listItem)
+    })
+}
+
+
+const setMovieById = function(movie) {
+  const moviesGenres = movieData.genres;
+  moviesGenres.forEach(movieGenre => {
+      const listItem = document.createElement('li');
+      const listItemContent = `genre naam: ${movieGenre.name}, id: ${movieGenre.id}`
+      listItem.appendChild(document.createTextNode(listItemContent));
+      document.getElementById("movieGenresList").append(listItem)
+  })
+};
